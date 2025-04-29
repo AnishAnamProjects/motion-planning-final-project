b5c29ce39b6a4ba5797b746999229b9464b8b6fe
Tyler Webster <tyler.webster1234@gmail.com>
2025-04-28 23:33:29 -0600

    Improved querying speed, added animation capability, probably made the surveying display better too. Can't remember

--- quadcopter_simulation/Driver.py
+++ quadcopter_simulation/Driver.py
@@ -6,8 +6,9 @@ from runsim import *
 import matplotlib.pyplot as plt
 from environment import Environment
 import open3d as o3d
+import time
 
-n_drones = 10
+n_drones = 3
 drones = [None] * n_drones  # Initialize a list to hold the drones
 waypoints = [None] * n_drones  # Initialize a list to hold the waypoints
 drone_states = [None] * n_drones  # Initialize a list to hold the drone states
@@ -15,8 +16,7 @@ drone_states = [None] * n_drones  # Initialize a list to hold the drone states
 #initialize plotter or the environment
 file = "resources/Industrial_full_scene_0.5hires_lessfloor.ply"
 goal = (10, 10, 10)
-industrial = Environment(goal_position=goal, filename=file, resolution=50)
-print(industrial.query((4,6,0)))
+industrial = Environment(goal_position=goal, filename=file, resolution=1)
 
 # intialize the unknown map
 knowledge_map = np.zeros(industrial.obstacle_map.shape)  # Initialize the knowledge map with zeros
@@ -43,10 +43,22 @@ for i in range(n_drones):
     waypoints[i] = np.random.uniform([industrial.x_min, industrial.y_min, industrial.z_min],
                                   [industrial.x_max, industrial.y_max, industrial.z_max], (10, 3))
 
+#define waypoint array of two points
+# waypoints = [np.array([[0, 0, 5,]])]
+
+
 # Execute the trajectory for each drone
 for i in range(n_drones):
     drones[i].execute_trajectory(waypoints[i])
     drone_states[i] = np.array(drones[i].states)  # Store the states of each drone
+
+# # do another set of waypoints
+# waypoints = [np.array([[-10, -10, 10]])]
+
+# # Execute the trajectory for each drone
+# for i in range(n_drones):
+#     drones[i].execute_trajectory(waypoints[i])
+#     drone_states[i] = np.array(drones[i].states)  # Store the states of each drone
 
 # Plot the trajectories of each drone
 drone_trajectory_pcd = [None] * n_drones  # Initialize a list to hold the point clouds for each drone
@@ -57,6 +69,30 @@ for i in range(n_drones):
     #drone_trajectory_pcd[i].paint_uniform_color([0, 0, 1])  # Set color to blue
     plotting_list.append(drone_trajectory_pcd[i])
 
-
+plotting_list.append(industrial.detected_obstacles)  # Add the knowledge map to the plotting list
+plotting_list.append(industrial.detected_free_space)
 o3d.visualization.draw_geometries(plotting_list)
 
+vis = o3d.visualization.Visualizer()
+vis.create_window()
+
+for i in range(n_drones):
+    vis.add_geometry(drone_trajectory_pcd[i])  # Add the point cloud for each drone to the visualizer
+
+
+vis.add_geometry(industrial.point_cloud)
+
+print(len(drones[0].states))
+# create an animation loop
+for steps in range(1, len(drones[0].states)):
+    #new_pcd.points = o3d.utility.Vector3dVector(drones[0].states[0:steps])
+    for i in range(n_drones):
+        # Add the point cloud for each drone to the visualizer
+        drone_trajectory_pcd[i].points = o3d.utility.Vector3dVector([drones[i].states[steps]])
+        vis.update_geometry(drone_trajectory_pcd[i])
+
+    vis.poll_events()
+    vis.update_renderer()
+    print(steps)
+    time.sleep(0.001)
+vis.destroy_window()
--- quadcopter_simulation/environment.py
+++ quadcopter_simulation/environment.py
@@ -3,7 +3,7 @@ import numpy as np
 from pointcloud import *
 
 class Environment:
-    def __init__(self, goal_position, filename, resolution=50):
+    def __init__(self, goal_position, filename, resolution=5):
         '''
         Initialize the environment with a point cloud
         '''
@@ -13,7 +13,7 @@ class Environment:
         # Create a coordinate frame
 
         self.point_cloud = o3d.io.read_point_cloud(self.file)
-        self.point_cloud.colors = o3d.utility.Vector3dVector([[1, 0, 0]] * len(self.point_cloud.points))  # Set color to gray
+        self.point_cloud.colors = o3d.utility.Vector3dVector([[0.5, 0.5, 0.5]] * len(self.point_cloud.points))  # Set color to gray
         
         # extract the limits of the point cloud
         self.x_min, self.x_max = np.min(np.asarray(self.point_cloud.points)[:, 0]), np.max(np.asarray(self.point_cloud.points)[:, 0])
@@ -26,13 +26,35 @@ class Environment:
         # define the grid resolution
         self.resolution = resolution
 
+        self.knowledge_map = o3d.geometry.VoxelGrid()
+        self.knowledge_map.voxel_size = resolution
         self.obstacle_map = self.generate_obstacle_map()
+        self.obstacle_map2 = self.generate_obstacle_map_voxels()
+        self.obstacle_voxel_list = self.obstacle_map2.get_voxels()
+        self.obstacle_voxel_list = [tuple(voxel.grid_index) for voxel in self.obstacle_voxel_list]
+        self.obstacle_voxel_list = set(self.obstacle_voxel_list)  # Convert to a set for faster lookup
+        self.detected_obstacles = o3d.geometry.VoxelGrid()
+        self.detected_obstacles.voxel_size = resolution
+       
+        self.detected_free_space = o3d.geometry.VoxelGrid()
+        self.detected_free_space.voxel_size = resolution
+
     def show(self, drone_trajectories=None):
         '''
         Show the point cloud in Open3D
         '''
+
+        # Make it a Voxel grid
+        voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(self.point_cloud, voxel_size=2)
+
+        # Add a new green voxel to the grid above the environment
+        goal_voxel = o3d.geometry.Voxel()
+        goal_voxel.grid_index = np.array([0, 0, 100])
+        goal_voxel.color = [0, 1, 0]  # Set color to green
+        voxel_grid.add_voxel(goal_voxel)
         # Visualize the point cloud
-        o3d.visualization.draw_geometries([self.point_cloud])
+        o3d.visualization.draw_geometries([self.point_cloud, voxel_grid])
+        
         
 
     def generate_obstacle_map(self):
@@ -47,29 +69,28 @@ class Environment:
 
         # Create a 3D histogram to count points in each voxel
         hist, edges = np.histogramdd(np.asarray(self.point_cloud.points), bins=(x_bins, y_bins, z_bins))
+
         obstacle_map = hist > 0
         return obstacle_map
-        
-    def query(self, voxel):
+    
+    def generate_obstacle_map_voxels(self):
+        return o3d.geometry.VoxelGrid.create_from_point_cloud(self.point_cloud, voxel_size=self.resolution)
+    
+    def query(self, point):
         '''
         Query the obstacle map for a given position
         '''
-
-        # Check if the voxel is within the bounds of the grid
-        if (voxel[0] < 0 or voxel[0] >= self.resolution or
-            voxel[1] < 0 or voxel[1] >= self.resolution or
-            voxel[2] < 0 or voxel[2] >= self.resolution):
-            return "Out of bounds"
-        # Check if the voxel is the goal position
-        elif np.all(voxel == self.world_to_grid(self.goal)):
-            return "Goal"
-        # check if the voxel is in the obstacle map
-        elif self.obstacle_map[voxel[0], voxel[1], voxel[2]]:
-            return "Obstacle"
-        # otherwise the voxel is free
+        voxel_index = self.obstacle_map2.get_voxel(point)
+        if voxel_index is not None:
+            if tuple(voxel_index) in self.obstacle_voxel_list:
+            #if any((voxel.grid_index == voxel_index).all() for voxel in self.obstacle_voxel_list):
+                return "Obstacle"
+            else:
+                return "Free" 
         else:
-            return "Free"    
-    
+            return "Out of bounds"
+        
+        
     def world_to_grid(self, pos):
         """Convert world coordinates to grid indices"""
         x = int((pos[0] - self.x_min) / (self.x_max - self.x_min) * self.resolution)
@@ -77,5 +98,5 @@ class Environment:
         z = int((pos[2] - self.z_min) / (self.z_max - self.z_min) * self.resolution)
         return (x, y, z)
 
-#industrial = Environment(goal_position=(10, 10, 10), filename="resources/Industrial_full_scene_0.5hires_lessfloor.ply", resolution=0.01)
+#industrial = Environment(goal_position=(10, 10, 10), filename="resources/Industrial_full_scene_0.5hires_lessfloor.ply", resolution=50)
 #industrial.show()
